a0cf526b2056011e4c34be7a58d064546fea8656
kelvinMulinge <kelvinmulinge9702@gmail.com>
2024-11-13 14:39:22 +0300

    Use Effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    async function fetchDogImage() {
+      try {
+        await fetch("https://dog.ceo/api/breeds/image/random")
+          .then((resp) => resp.json())
+          .then((data) => setImage(data.message));
+      } catch (error) {
+        console.error("Error resolving this page", error);
+      } finally {
+        setloading(false);
+      }
+    }
+    fetchDogImage();
+  }, []);
+  return (
+    <div>
+      {loading ? <p>Loading...</p> : <img src={image} alt="A Random Dog" />}
+    </div>
+  );
+}
+export default App;
